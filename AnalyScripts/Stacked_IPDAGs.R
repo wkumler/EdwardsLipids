689c94194c39bc4ba981530e7b96f38f04defc08
wkumler <wkumler@berkeley.edu>
2018-09-14 10:59:29 -0700

    Updated Stacked_IPDAGs

--- AnalyScripts/Stacked_IPDAGs.R
+++ AnalyScripts/Stacked_IPDAGs.R
@@ -1,9 +1,9 @@
-# Piecharts.R
+# Stacked_IPDAGs.R
 
 # Code designed to make Figure 2 of the poster
 
 # Input: Clean_Complete.csv
-# Output: Stacked.png, 
+# Output: Stacked_IPDAGs.png, 
 
 # Startup things ----
 library(dplyr)
@@ -70,8 +70,22 @@ classify.MvP <- function(station.number) {
 norm.rel.IPDAGs <- mutate(norm.rel.IPDAGs, "Location"=classify.MvP(Station))
 
 norm.rel.IPDAGs %>% group_by(Station) %>%
-  ggplot(aes(x=Station, y=proportion)) + 
-  geom_bar(aes(fill=species), stat = "identity") +
-  facet_wrap(~Location, scales = "free_x")
+  ggplot(aes(x=Station, y=proportion, fill=species)) + 
+  geom_bar(stat = "identity") +
+  geom_bar(color="black", stat = "identity", show.legend = F) + #second call to
+    #geom_bar to draw outlines without outlining legend boxes
+  facet_wrap(~Location, scales = "free_x") + #hella clever, thanks internet
+  guides(fill = guide_legend(nrow = 1)) +
+  ylab("Relative proportion of IP-DAG species") +
+  xlab("Station Number") + 
+  theme(legend.title=element_blank(), legend.position = "bottom",
+      axis.text = element_text(size = 32, face = "bold"),
+      axis.title = element_text(size = 32, face = "bold"),
+      legend.text = element_text(size = 32, face = "bold")) +
+  scale_fill_discrete(breaks = c("DGDG", "DGTS_DGTA", "MGDG", "PC", "PE",
+                                 "PG", "SQDG", "DGCC"),
+                      labels = paste0(" ", c("DGDG", "DGTS/DGTA", "MGDG", "PC", 
+                                            "PE", "PG", "SQDG", "DGCC"), "     "))
 
-ggsave(filename = "Stacked_IPDAGs.png", device = "png", path = "Images")
+ggsave(filename = "Stacked_IPDAGs.png", device = "png", path = "Images", 
+       width = 8, height = 8, units = "in")
